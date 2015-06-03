fb343cabc4050255b9414ecf7ad90b23c824ac63
Juanito Fatas <katehuang0320@gmail.com>
2015-06-03 15:21:21 +0800

    Add a 'Tap to start' label

--- app/main_activity.rb
+++ app/main_activity.rb
@@ -1,5 +1,14 @@
 class MainActivity < Android::App::Activity
   def onCreate(savedInstanceState)
     super
+
+    layout = Android::Widget::LinearLayout.new(self)
+    layout.orientation = Android::Widget::LinearLayout::VERTICAL
+
+    @label = Android::Widget::TextView.new(self)
+    @label.text = 'Tap to start'
+    @label.textSize = 80.0
+    @label.gravity = Android::View::Gravity::CENTER_HORIZONTAL
+    layout.addView(@label)
   end
 end
